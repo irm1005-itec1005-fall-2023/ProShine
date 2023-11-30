78bcc646cfe96f83c50a61a442cab7a226295bde
Ahmed ABdulwahid <ahmedabdulwahid@cmail.carleton.ca>
2023-11-29 20:40:02 -0500

    assignment 04

--- script.js
+++ script.js
@@ -1,43 +1,59 @@
-/* Assignment 04: Finishing a Todo List App
- *
- * 
- *
- */
+
+const inputbox = document.getElementById("input-box");
+
+const listContainer = document.getElementById("list-countainer");
+
+const emptyState = document.getElementById("empty-state");
 
 
-//
-// Variables
-//
+function addTask(){
+  if(inputbox.value === ''){
+    alert("Please enter a task!");
+  }
+  else{
+   let li = document.createElement("li");
+   li.innerHTML = inputbox.value;
+   listContainer.appendChild(li);
+   let span = document.createElement("span");
+   span.innerHTML = "\u00d7";
+   li.appendChild(span);
 
-// Constants
-const appID = "app";
-const headingText = "To do. To done. ✅";
+   }
 
-// DOM Elements
-let appContainer = document.getElementById(appID);
+   inputbox.value = '';
 
-//
-// Functions
-//
+   storeData();
+   updateEmptyState();
+} 
 
-// Add a heading to the app container
-function inititialise() {
-  // If anything is wrong with the app container then end
-  if (!appContainer) {
-    console.error("Error: Could not find app contianer");
-    return;
+listContainer.addEventListener('click', function(e){
+  if(e.target.tagName === 'LI'){
+    e.target.classList.toggle('checked');
+    storeData();
+  }
+  else if(e.target.tagName === 'SPAN'){
+    var div = e.target.parentNode;
+    div.remove();
+    storeData();
   }
 
-  // Create an h1 and add it to our app
-  const h1 = document.createElement("h1");
-  h1.innerText = headingText;
-  appContainer.appendChild(h1);
 
-  // Init complete
-  console.log("App successfully initialised");
+  updateEmptyState();
+}, false);
+
+
+function storeData(){
+  localStorage.setItem("data", listContainer.innerHTML);
 }
 
-//
-// Inits & Event Listeners
-//
-inititialise();
+function saveTask(){
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+saveTask();
+updateEmptyState();
+
+function updateEmptyState() {
+  emptyState.style.display = listContainer.children.length === 0 ? 'block' : 'none';
+  emptyState.classList.toggle('hiding', listContainer.children.length > 0);
+}
